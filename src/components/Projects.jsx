18e12f4367c7072945c1293e722091b8f6abaf6c
Sachin_2018 <sachinrajum82@gmail.com>
2026-05-14 21:20:49 +0530

    Update padding and layout for Projects and Testimonials components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -31,7 +31,7 @@ const Projects = () => {
       transition={{duration: 1}}
       whileInView={{opacity:1, x:0}}
       viewport={{once:true}}
-    className='container mx-auto py-4 pt-40 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
+    className='container mx-auto py-4 pt-40 px-6 md:px-20 lg:px-16 my-20 w-full overflow-hidden' id='Projects'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
       <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting Space, Building Legacies-Explore Our Projects</p>
       {/* Slider buttons */}
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,10 +8,9 @@ const Testimonials = () => {
       transition={{duration: 1}}
       whileInView={{opacity:1, x:0}}
       viewport={{once:true}}
-    className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
-      <h1 className='text-2xl sm:text-44xl font-bold mb-2 text-center'>Customer <span className='underline underline-offset-4 decoration-1 under font-light'>
-        Testimonials</span></h1>
-      <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Stories from Those Who Found Home With Us </p>
+    className='container mx-auto py-10 lg:px-16 w-full overflow-hidden' id='Testimonials'>
+      <h1 className='text-2xl sm:text-44xl font-bold mb-2 text-center'>Customer Testimonials</h1>
+      <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Stories from Those Who Found Home </p>
       <div className='flex flex-wrap justify-center gap-8'>
         {testimonialsData.map((testimonial, index) => (
           <div key={index} className='max-w-[340px] border shadow-lg rounded px-8 py-12 text-center'>
