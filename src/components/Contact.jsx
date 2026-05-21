58a1a599901d5b88d13f1214c81506b1f5895540
Sachin_2018 <sachinrajum82@gmail.com>
2026-05-21 19:56:08 +0530

    Fix spacing in Contact header for improved readability

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
       whileInView={{opacity:1, x:0}}
       viewport={{once:true}}
     className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
-      <h1 className='text-2xl sm:text-44xl font-bold mb-2 text-center'>Contact<span className='underline underline-offset-4 decoration-1 under font-light'>
+      <h1 className='text-2xl sm:text-44xl font-bold mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>
         with us</span></h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Stories from Those Who Found Home With Us </p>
       <form onSubmit={onSubmit} className='max-w-2xl mx-auto text-gray-600 pt-8'>
